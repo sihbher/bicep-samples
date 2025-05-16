8b722fad166c580f39c132513ba786d1ae971a5c
Gerardo Reyes <epalnemohuani@hotmail.com>
2025-05-16 13:11:10 -0400

    AKS example comments

--- aks-deployment/aks-rg.bicep
+++ aks-deployment/aks-rg.bicep
@@ -1,13 +1,27 @@
+// This template deploys a resource group at the subscription level
+// targetScope must be 'subscription' to create resource groups
 targetScope = 'subscription'
 
-
+// Parameters
+// rgName: Name of the resource group to be created
+// Default follows Azure naming convention: rg-<workload>-<environment>-<region>-<instance>
 param rgName string = 'rg-aks-prod-eastus-001'
+
+// location: Azure region where the resource group will be deployed
+// This region should be chosen based on latency, data sovereignty, or service availability requirements
 param location string = 'eastus'
 
+// Deploy a resource group using the Azure Verified Module (AVM)
+// AVM modules implement Microsoft's recommended best practices for Azure resources
 module ResourceGroup 'br/public:avm/res/resources/resource-group:0.4.1' = {
-  name: 'myRg'
+  name: 'myRg' // Name of the deployment operation (not the resource group name)
   params: {  
-    name: rgName  
-    location: location  
+    name: rgName  // Actual resource group name passed from parameters
+    location: location  // Azure region for the resource group
+    // Note: Additional parameters like tags, lock settings, and role assignments
+    // can be added here if needed for governance and management
   }
 }
+
+// No outputs are defined, but you could add resource group ID or other properties
+// if you need them for subsequent deployments
--- aks-deployment/deploy.sh
+++ aks-deployment/deploy.sh
@@ -1,21 +1,34 @@
 
-#Login with code
-az login --tenant --use-device-code
+# Login with device code authentication
+# This is useful for environments where interactive login isn't available
+az login --use-device-code
 
-#name of the resource group
+# Set the base name for your resource group
 rg_name=aks-rg
+
+# Set the Azure region for deployment
 location='eastus2'
-#random suffix to be used, 6 letters
+
+# Generate a random 6-character suffix for unique resource naming
+# This prevents naming conflicts when deploying multiple instances
 suffix=$(openssl rand -hex 3)
+
+# Create the final resource group name with suffix
 rg_name_with_suffix=$rg_name-$suffix
 
+# Navigate to the directory containing deployment templates
 cd aks-deployment
-#First deploy the resource group
+
+# First
+# Deploy the resource group using subscription-level deployment
+# This creates a new resource group with a unique name
 az deployment sub create --name 'rg-creation' --location $location \
-    --template-file './aks-rg.bicep'  \
+    --template-file './aks-rg.bicep' \
     --parameters rgName=$rg_name_with_suffix location=$location
 
-#Second deploy the AKS cluster
+# Second
+# Deploy the AKS cluster within the resource group
+# This uses both parameter file values and command-line parameters
 az deployment group create --name aks-$suffix \
     --resource-group $rg_name_with_suffix \
     --template-file './aks-deployment.bicep' \
